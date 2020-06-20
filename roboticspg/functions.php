0829681496636c8cb2d2d97914ddc9413a1ff279
theTruffles <anna_truffet@hotmail.com>
2020-06-20 12:23:32 +1000

    Refactored adding custom post attrs, added custom page attrs (i.e. page color)

--- roboticspg/functions.php
+++ roboticspg/functions.php
@@ -8,7 +8,7 @@ define("WHITE_DARK", "DFE4E5");
 define("YELLOW_LIGHT", "E59B38");
 define("YELLOW_DARK", "D38F34");
 define("AQUA_LIGHT", "46B5BB");
-define("AQUA_DARK", "DFE4E5");
+define("AQUA_DARK", "42a9ae");
 define("PINK_LIGHT", "FAD6E2");
 define("PINK_DARK", "EBC9D4");
 define("BLUE_LIGHT", "1D49B5");
@@ -413,27 +413,62 @@ function add_custom_color_palette()
 	Additional Attrs for Pages & Posts
 \*------------------------------------*/
 
-// POST SORTING ON A PAGE
-function jpen_custom_post_sort($post)
+/** adds the meta box in side bar */
+function add_custom_meta_boxes($post)
 {
+
+    // add meta box for page
     add_meta_box(
-        'custom_post_sort_box',
+        'page_color_meta_box', // id, used as the html id att
+        __('Page Color'), // meta box title
+        'page_color_meta_box_options', // callback function, spits out the content
+        'page', // add to page
+        'side', // context (where on the screen)
+        'high' // priority
+    );
+
+    // add meta box for post
+    add_meta_box(
+        'post_order_theme_meta_box',
         'Post Order & Theme',
-        'jpen_custom_post_order',
-        'post',
+        'post_order_theme_meta_box_options',
+        'post', // add to posts
         'side'
     );
 }
 
-function jpen_custom_post_order($post)
+/** adds drop down for for custom page attrs in side bar */
+function page_color_meta_box_options($post)
 {
-    wp_nonce_field(basename(__FILE__), 'jpen_custom_post_order_nonce');
+    wp_nonce_field(basename(__FILE__), 'page_color_meta_box_options_nonce');
+
+    ?>
+        <p>Page Color:</p>
+        <p>
+            <select name="page_color">
+                <option value="blue-light">Blue</option>
+                <option value="aqua-light">Aqua</option>
+                <option value="yellow-dark">Yellow</option>
+                <option value="pink-light">Pink</option>
+            </select>
+        </p>
+
+    <?php
+}
+
+/** adds drop down for for custom post attrs in side bar */
+function post_order_theme_meta_box_options($post)
+{
+    wp_nonce_field(basename(__FILE__), 'post_order_theme_meta_box_options_nonce');
+
     $current_pos = get_post_meta($post->ID, '_custom_post_order', true); ?>
+        <!-- post order on page -->
         <p>Post's position to appear in. E.g. post "1" will appear first, post "2" second.</p>
         <p><input type="number" name="pos" value="<?php echo $current_pos; ?>" /></p>
 
         </br>
 
+        <!-- post theme -->
         <p>White Themed or Colored Themed?</p>
         <p>
             <select name="theme">
@@ -444,24 +479,39 @@ function jpen_custom_post_order($post)
     <?php
 }
 
-function jpen_save_custom_post_order($post_id)
+/** TRUE iff user can save custom attrs */
+function can_save_custom_attrs($post_id, $post_attr_name, $attr_name, $capability)
 {
-    if (!isset($_POST['jpen_custom_post_order_nonce']) || !wp_verify_nonce($_POST['jpen_custom_post_order_nonce'], basename(__FILE__))) {
-        return;
+    if (!isset($_POST[$post_attr_name]) || !wp_verify_nonce($_POST[$post_attr_name], basename(__FILE__))) {
+        return FALSE;
+    } else if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return FALSE;
+    } else if (!current_user_can($capability, $post_id)) {
+        return FALSE;
     }
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return;
-    }
-    if (!current_user_can('edit_post', $post_id)) {
-        return;
-    }
-    if (isset($_REQUEST['pos']) && isset($_REQUEST['theme'])) {
+
+    return isset($_REQUEST[$attr_name]);
+}
+
+/** saves custom post attrs if possible */
+function jpen_save_custom_post_attrs($post_id)
+{
+    if (can_save_custom_attrs($post_id, 'post_order_theme_meta_box_options_nonce', 'pos', 'edit_post')) {
         update_post_meta($post_id, '_custom_post_order', sanitize_text_field($_POST['pos']));
         update_post_meta($post_id, '_custom_post_theme', sanitize_text_field($_POST['theme']));
     }
 }
 
-function jpen_add_custom_post_order_column($columns)
+/** saves custom page attrs if possible */
+function jpen_save_custom_page_attrs($post_id)
+{
+    if (can_save_custom_attrs($post_id, 'page_color_meta_box_options_nonce', 'page_color', 'edit_page')) {
+        update_post_meta($post_id, '_custom_page_color', sanitize_text_field($_POST['page_color']));
+    }
+}
+
+/** adds the summary columns for displaying to admin */
+function add_custom_post_attrs_column($columns)
 {
     return array_merge(
         $columns,
@@ -469,15 +519,31 @@ function jpen_add_custom_post_order_column($columns)
     );
 }
 
-function jpen_custom_post_order_value($column, $post_id)
+/** adds the summary columns for displaying to admin */
+function add_custom_page_attrs_column($columns)
+{
+    return array_merge(
+        $columns,
+        array('page_color' => 'Page Color')
+    );
+}
+
+
+/** populates custom attrs in summary page */
+function display_custom_attr_in_columns($column, $post_id)
 {
     if ($column == 'pos') {
         echo '<p>' . get_post_meta($post_id, '_custom_post_order', true) . '</p>';
+
     } else if ($column == 'theme') {
         echo '<p>' . get_post_meta($post_id, '_custom_post_theme', true) . '</p>';
+
+    } else if ($column == 'page_color') {
+        echo '<p>' . get_post_meta($post_id, '_custom_page_color', true) . '</p>';
     }
 }
 
+/** automatically sorts posts when calling get_posts when constructing the page. */
 function jpen_custom_post_order_sort($query)
 {
     if ($query->is_main_query() && is_home()) {
@@ -487,13 +553,14 @@ function jpen_custom_post_order_sort($query)
     }
 }
 
+// PAGE COLOR ATTR
+add_action('save_page', 'jpen_save_custom_page_attrs');
+add_filter('manage_pages_columns', 'add_custom_page_attrs_column'); // Sort custom post order in admin view
+add_action('manage_pages_custom_column', 'display_custom_attr_in_columns', 10, 2);
+
 /*------------------------------------*\
     CUSTOM POST TYPES
 \*------------------------------------*/
-
-
-//add_post_type_support('page', 'post-formats');
-
 
 // register custom post type 'my_custom_post_type' with 'supports' parameter
 add_action('init', 'center-banner');
@@ -509,6 +576,7 @@ function create_my_post_type()
         )
     );
 }
+
 
 /*------------------------------------*\
 	Actions + Filters + ShortCodes
@@ -528,11 +596,10 @@ add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post
 add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
 add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
 add_action('after_setup_theme', 'add_custom_color_palette'); // add color palette
-add_action('add_meta_boxes', 'jpen_custom_post_sort'); // add custom post sorting
-add_action('save_post', 'jpen_save_custom_post_order'); // add custom post saving order
-add_action('manage_posts_custom_column', 'jpen_custom_post_order_value', 10, 2); // display custom post order in admin view
+add_action('add_meta_boxes', 'add_custom_meta_boxes'); // add custom post sorting
+add_action('save_post', 'jpen_save_custom_post_attrs'); // add custom post saving order
+add_action('manage_posts_custom_column', 'display_custom_attr_in_columns', 10, 2); // display custom post order in admin view
 add_action('pre_get_posts', 'jpen_custom_post_order_sort'); // get posts so they are custom sorted
-add_action('manage_posts_custom_column', 'jpen_custom_post_order_value', 10, 2);
 
 // Remove Actions
 remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
@@ -565,7 +632,7 @@ add_filter('show_admin_bar', 'remove_admin_bar'); // Remove Admin bar
 add_filter('style_loader_tag', 'html5_style_remove'); // Remove 'text/css' from enqueued stylesheet
 add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to thumbnails
 add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to post images
-add_filter('manage_posts_columns', 'jpen_add_custom_post_order_column'); // Sort custom post order in admin view
+add_filter('manage_posts_columns', 'add_custom_post_attrs_column'); // Sort custom post order in admin view
 
 
 // Remove Filters
